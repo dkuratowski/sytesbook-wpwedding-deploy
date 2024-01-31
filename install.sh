9305baaa7ca80b02e113e4f37c06c3e5b5ec9c6f
David Moldvai <64195763+dkuratowski@users.noreply.github.com>
2024-01-31 14:00:15 +0100

    Output of http requests to install scripts handled properly

--- install.sh
+++ install.sh
@@ -5,12 +5,34 @@ deployment_id=$2
 deployment_folder=$3
 domain_folder=$4
 
-# echo "Unzipping deployed files"
-# mapfile output < <(curl --silent --write-out "\n%{response_code}" "https://$domain_name/installer/$deployment_id/unzip.php?deployment_folder=$deployment_folder")
-# echo ${output[@]}
+echo "Unzipping deployed files"
+mapfile output < <(curl --silent --write-out "\n%{response_code}" "https://$domain_name/installer/$deployment_id/unzip.php?deployment_folder=$deployment_folder")
+output_length=${#output[@]}
+response_code="${output[$(($output_length - 1))]}"
+last_line_index=$(($output_length - 2))
+if [[ $response_code != "200" ]]; then
+    echo "Failed"
+    for (( i=0; i<=$last_line_index; i++))
+    do
+        echo "${output[$i]}";
+    done
+    exit 1
+fi
+echo "OK"
 
 echo "Creating symbolic links"
 mapfile output < <(curl --silent --write-out "\n%{response_code}" "https://$domain_name/installer/$deployment_id/symlink.php?deployment_folder=$deployment_folder&domain_folder=$domain_folder")
-echo ${output[@]}
+output_length=${#output[@]}
+response_code="${output[$(($output_length - 1))]}"
+last_line_index=$(($output_length - 2))
+if [[ $response_code != "200" ]]; then
+    echo "Failed"
+    for (( i=0; i<=$last_line_index; i++))
+    do
+        echo "${output[$i]}";
+    done
+    exit 1
+fi
 
+echo "OK"
 exit 0
